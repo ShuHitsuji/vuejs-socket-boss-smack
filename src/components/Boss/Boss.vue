d80276df3cdb11bc6ddaf0a4774fe43f867a5f86
FranORT <54158944+FranORT@users.noreply.github.com>
2019-10-24 21:02:49 -0300

    Modified Boss Folder
    Added Boss.vue file

---
+++ src/components/Boss/Boss.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bossSprite">
+    <div class="bossImage">
+      <img v-show="currentHeroState == 'Idle'" src="Placeholder">
+      <img v-show="currentHeroState == 'Attacking'" src="Placeholder">
+      <img v-show="currentHeroState == 'Special'" src="Placeholder">
+      <img v-show="currentHeroState == 'Healing'" src="Placeholder">
+      <img v-show="currentHeroState == 'Hurt'" src="Placeholder">
+    </div>    
+  </div>
+</template>
+
+<script>
+    <!-- TODO -->
+</script>
