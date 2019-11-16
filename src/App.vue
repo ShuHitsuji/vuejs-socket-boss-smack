ce6e3aaa0cda080af412ce1c0ec88012ec37e9f8
Guillermo Perez <gperez78@gmail.com>
2019-11-15 22:24:44 -0300

    transition duration

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
 
   .fade-enter-active,
   .fade-leave-active {
-    transition-duration: 0.3s;
+    transition-duration: 0.5s;
     transition-property: opacity;
     transition-timing-function: ease;
   }
